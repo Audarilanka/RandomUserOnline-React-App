aa2349ec4a81b6340763361ec745d988968420c4
Audarilanka <dissanayakelanka96@gmail.com>
2022-06-21 14:21:14 +0530

    feat(project)randomUser

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React     from "react";
-import NameList  from "./components/NameList/NameList";
-import HeaderBar from "./components/Headerbar/HeaderBar";
-import Home      from "./components/pages/Home/Home";
-import About     from "./components/pages/About/About";
+import React                            from "react";
+import NameList                         from "./components/NameList/NameList";
+import HeaderBar                        from "./components/Headerbar/HeaderBar";
+import Home                             from "./components/pages/Home/Home";
+import About                            from "./components/pages/About/About";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 //import "./App.css";
 
 function App() {
   return (
-    <div>
-      <HeaderBar />
-      <Home />
-      <About />
-      <NameList />
-    </div>
+    <BrowserRouter>
+      <div>
+        <HeaderBar />
+        <Routes>
+          <Route path="/" element={<NameList />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Headerbar/HeaderBar.jsx
+++ src/components/Headerbar/HeaderBar.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React    from "react";
+
+
+import { Link } from "react-router-dom";
 
 export default function HeaderBar() {
   return (
@@ -6,21 +9,27 @@ export default function HeaderBar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
         <span className="navbar-brand mb-0 h1">Name List</span>
         <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Show Name List
-            </a>
-          </li>
+          <Link to="/home">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+          </Link>
+          <Link to="/">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Show Name List
+              </a>
+            </li>
+          </Link>
         </ul>
       </nav>
     </div>
